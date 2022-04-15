3186dbb5f455c701d0217d70df01bf631943c069
Maikel de Laat <devtools@maikeldelaat.nl>
2022-04-15 14:23:15 +0200

    no message

--- Maikelsoft.Monads/ExtendOptional.cs
+++ Maikelsoft.Monads/ExtendOptional.cs
@@ -7,25 +7,16 @@ namespace Maikelsoft.Monads
     {
         [Pure]
         public static T? GetValueOrDefault<T>(this Optional<T> optional)
-            //where T : notnull
             where T : struct
         {
             return optional.GetValueOrDefault(default);
-            //return optional.Match(() => default, v => (T?)v);
         }
         
-        //[Pure]
-        //public static T? GetValueOrNull<T>(this Optional<T> optional)
-        //    where T : struct
-        //{
-        //    return optional.Match(() => (T?)null, value => value);
-        //}
-
         [Pure]
         public static T? GetValueOrNull<T>(this Optional<T> optional)
             where T : class
         {
-            return optional.Match(() => default, value => (T?)value);
+            return optional.Match(() => null, value => (T?)value);
         }
 
         public static void DiposeValue<T>(this Optional<T> optional)
---
+++ Maikelsoft.Monads/ExtendTry.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace Maikelsoft.Monads
+{
+    public static class ExtendTry
+    {
+        [Pure]
+        public static T? GetValueOrDefault<T>(this Try<T> optional)
+            where T : struct
+        {
+            return optional.GetValueOrDefault(default);
+        }
+        
+        [Pure]
+        public static T? GetValueOrNull<T>(this Try<T> optional)
+            where T : class
+        {
+            return optional.Match(_ => null, value => (T?)value);
+        }
+    }
+}
